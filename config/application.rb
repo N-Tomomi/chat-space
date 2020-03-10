1abe9ffdc354f0e0cc42d2460f3506b738da80ab
N-Tomomi <d_o_yukijiro@hotmail.co.jp>
2020-03-10 12:50:02 +0900

    Ajax
    -Ajax

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module ChatSpace
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.time_zone = 'Tokyo'
+
     config.generators do |g|
       g.stylesheets false
       g.javascripts false
@@ -19,5 +21,6 @@ module ChatSpace
     end
     
     config.i18n.default_locale = :ja
+
   end
 end
